0dd0b6b19ea4ce56dd7492528ca2b6e48d1619ce
Artur <artur.prado@gmail.com>
2018-08-21 14:18:02 -0300

    Build challenge structure

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -1,4 +1,11 @@
 class ChallengesController < ApplicationController
   def index
   end
+
+  def create
+    @verb = Verb.all.sample
+    @verb_tense = VerbTense.all.sample
+
+    render :index
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :challenges, only: :index
+  resources :challenges, only: [:index, :create]
 
   get 'conjugate/(:verb)', only: :show, controller: :conjugations, action: :show, as: :conjugation
 
